abf9e5c0c60130069b5b6823c5957b42ef1829c8
Carlos0127 <carloscl01@hotmail.com>
2025-05-19 23:53:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # URLs de los archivos CSV
-clientes_url = "https://raw.githubusercontent.com/Carlos0127/Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/clientes.csv"
-mercados_url = "https://raw.githubusercontent.com/Carlos0127/Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/mercados.csv"
-exportaciones_url = "https://raw.githubusercontent.com/Carlos0127/Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/exportaciones.csv"
-barreras_url = "https://raw.githubusercontent.com/Carlos0127/Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/barreras.csv"
-
+clientes_url = "https://raw.githubusercontent.com/Carlos0127/-Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/clientes.csv"
+mercados_url = "https://raw.githubusercontent.com/Carlos0127/-Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/mercados.csv"
+exportaciones_url = "https://raw.githubusercontent.com/Carlos0127/-Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/exportaciones.csv"
+barreras_url = "https://raw.githubusercontent.com/Carlos0127/-Dashboard-Chocolate-Export-/ad2ca5ec865ebc2b68b80a7cd70d98b18476191d/barreras.csv"
 clientes = pd.read_csv(clientes_url)
 mercados = pd.read_csv(mercados_url)
 exportaciones = pd.read_csv(exportaciones_url)
